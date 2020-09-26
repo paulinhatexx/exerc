b40769940f01c6e7de125dd0575473f03ea6e549
paulinhatexx <paulinha.texx@hotmail.com>
2020-09-26 22:11:58 +0000

    add lista2ex3.c

---
+++ lista2ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//3. Escreva um programa em C que leia um número inteiro e classifique-o como par ou ímpar, 
+//dizendo ainda se este número é positivo, negativo ou nulo.
+int main (void){
+    int num;
+
+    printf("Lista 2 - Exercício 3\n");
+    printf("Digite um número inteiro:\n");
+    scanf("%d", &num);
+    return 0;
+}
