6116f76ec75ea1417cfaa2e0389418dc80d50948
paulinhatexx <paulinha.texx@hotmail.com>
2020-09-28 02:02:08 +0000

    update lista2ex3.c

--- lista2ex3.c
+++ lista2ex3.c
@@ -5,7 +5,21 @@ int main (void){
     int num;
 
     printf("Lista 2 - Exercício 3\n");
-    printf("Digite um número inteiro:\n");
+    printf("Digite um número inteiro: ");
     scanf("%d", &num);
+    if (num % 2==0){
+        printf("O número %d é par\n", num);
+    } else{
+        printf("O número %d é ímpar\n", num);
+    }
+    if (num > 0){
+        printf("O número %d é positivo.\n" , num);
+    } else if (num < 0){
+        printf("O número %d é negativo\n", num);
+    } else{
+        printf("O número %d é nulo\n" , num);
+    }
+
     return 0;
+
 }
